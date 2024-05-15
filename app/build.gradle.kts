64d3b74229e6d1518c5bfb1005ab0c28cb6964b6
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2024-05-14 22:33:36 -0400

    Agregando las dependendencias necesarias para que el programa funciona

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -30,17 +31,17 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.8"
     }
     packaging {
         resources {
@@ -53,12 +54,26 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation (libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.activity.compose)
+
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    //navegacion
+    implementation(libs.androidx.navigation.compose)
+
+    //Room
+    implementation(libs.androidx.room.runtime)
+    annotationProcessor(libs.androidx.room.compiler)
+    ksp(libs.androidx.room.compiler)
+
+    implementation(libs.androidx.room.ktx)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/edu/ucne/registrotecnicos/MainActivity.kt
+++ app/src/main/java/com/edu/ucne/registrotecnicos/MainActivity.kt
@@ -4,44 +4,83 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.room.Room
+
+import com.edu.ucne.registrotecnicos.data.local.database.TecnicoDb
+import com.edu.ucne.registrotecnicos.data.local.entities.TecnicoEntity
+import com.edu.ucne.registrotecnicos.data.repository.TecnicoRepository
+import com.edu.ucne.registrotecnicos.presentation.tecnico.TecnicoListScreen
+import com.edu.ucne.registrotecnicos.presentation.tecnico.TecnicoScreen
+import com.edu.ucne.registrotecnicos.presentation.tecnico.TecnicoViewModel
 import com.edu.ucne.registrotecnicos.ui.theme.RegistroTecnicosTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var tecnicoDb: TecnicoDb
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        tecnicoDb = Room.databaseBuilder(
+            this,
+            TecnicoDb::class.java,
+            "Tecnico.db"
+        )
+            .fallbackToDestructiveMigration()
+            .build()
+
+        val repository = TecnicoRepository(tecnicoDb.tecnicoDao())
         enableEdgeToEdge()
         setContent {
             RegistroTecnicosTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Surface {
+                    val viewModel: TecnicoViewModel = viewModel(
+                        factory = TecnicoViewModel.provideFactory(repository)
                     )
+                    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(innerPadding)
+                                .padding(8.dp)
+                        ) {
+                            TecnicoScreen(viewModel = viewModel)
+
+                            TecnicoListScreen(
+                                viewModel = viewModel,
+                                onDeleteTecnico = { tecnico ->
+                                    LaunchedEffect(Unit) {
+                                        viewModel.deleteTecnico(tecnico)
+                                    }
+                                },
+                                onVerTecnico = {}
+                            )
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun Preview(){
     RegistroTecnicosTheme {
-        Greeting("Android")
+
     }
 }
