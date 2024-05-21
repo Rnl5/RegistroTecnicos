a1ebb43d1b2e8c70a9724789d0e92f9594a267c2
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2024-05-21 13:27:33 -0400

    Agregando el TipoTecnicoEntity en la base de datos

--- app/src/main/java/com/edu/ucne/registrotecnicos/data/local/database/TecnicoDb.kt
+++ app/src/main/java/com/edu/ucne/registrotecnicos/data/local/database/TecnicoDb.kt
@@ -3,16 +3,21 @@ package com.edu.ucne.registrotecnicos.data.local.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.edu.ucne.registrotecnicos.data.local.dao.TecnicoDao
+import com.edu.ucne.registrotecnicos.data.local.dao.TipoTecnicoDao
 import com.edu.ucne.registrotecnicos.data.local.entities.TecnicoEntity
+import com.edu.ucne.registrotecnicos.data.local.entities.TipoTecnicoEntity
 
 @Database(
     entities = [
-        TecnicoEntity::class
+        TecnicoEntity::class,
+        TipoTecnicoEntity::class
     ],
-    version = 1,
+    version = 3,
     exportSchema = false
 )
 
 abstract class TecnicoDb : RoomDatabase(){
     abstract fun tecnicoDao(): TecnicoDao
+
+    abstract fun tipoTecnicoDao(): TipoTecnicoDao
 }
