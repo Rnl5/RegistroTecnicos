a30e79a978ac467f6c0f9043442ab4ad2fedd1fa
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2024-05-21 13:26:29 -0400

    Modificando el TopAppBar para poder desplegarlo al dar click en el

--- app/src/main/java/com/edu/ucne/registrotecnicos/presentation/components/TopAppBar.kt
+++ app/src/main/java/com/edu/ucne/registrotecnicos/presentation/components/TopAppBar.kt
@@ -3,16 +3,26 @@ package com.edu.ucne.registrotecnicos.presentation.components
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.navigation.NavHostController
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopAppBar(title:String){
+fun TopAppBar(
+    title: String,
+    onTopBarClick: () -> Unit
+) {
     CenterAlignedTopAppBar(
         title = {
             Text(
@@ -22,7 +32,7 @@ fun TopAppBar(title:String){
             )
         },
         navigationIcon = {
-            IconButton(onClick = {}) {
+            IconButton(onClick = { onTopBarClick() }) {
                 Icon(
                     imageVector = Icons.Filled.Menu,
                     contentDescription = "Localized description"
